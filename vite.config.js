ac82b32f4ccfc266f28c7f7052cf5a96e9b159cc
ftq <ftquiocho@gmail.com>
2022-09-27 20:18:57 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/https://ftquiocho.github.io/qui8/" : "/",
+  base: '/qui8/',
   plugins: [vue()],
   resolve: {
     alias: {
