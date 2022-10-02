07f77e5bb89ef8a551e02f7fb97eeb268f9a42d7
ftquiocho <112052587+ftquiocho@users.noreply.github.com>
2022-10-02 09:54:48 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// import VModal from 'vue-js-modal'
+import VModal from 'vue-js-modal/dist/index.nocss.js'
+
+import VueImg from 'v-img'
+import { vfmPlugin } from "vue-final-modal"
+import VueSmoothScroll from 'vue2-smooth-scroll'
+Vue.use(VueSmoothScroll)
+
+
+
+
+
+Vue.use(VModal)
+Vue.use(VueImg)
+Vue.use(vfmPlugin)
+
+import './assets/base.css'
+import './assets/main.css'
+
+import 'hooper/dist/hooper.css';
+import 'vue-js-modal/dist/styles.css'
+
+
+
+
+new Vue({
+  render: (h) => h(App)
+}).$mount('#app')
